94946b196187a94f423085324cd8deb39281e308
devrouis <80670036+devrouis@users.noreply.github.com>
2022-12-26 07:40:21 -0500

    initial proxy project

--- map_details.py
+++ map_details.py
@@ -2,30 +2,86 @@ import requests
 from bs4 import BeautifulSoup
 import csv 
 import time
+import mysql.connector
+import numpy
+from lxml.html import fromstring
+from itertools import cycle
 
 from selenium import webdriver
+
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+import traceback
+def get_proxies():
+	url = 'https://free-proxy-list.net/'
+	response = requests.get(url)
+	parser = fromstring(response.text)
+	proxies = set()
+	for i in parser.xpath('//tbody/tr'):
+		if i.xpath('.//td[7][contains(text(),"yes")]'):
+			proxy = ":".join([i.xpath('.//td[1]/text()')[0], i.xpath('.//td[2]/text()')[0]])
+			proxies.add(proxy)
+	return proxies
+#If you are copy pasting proxy ips, put in the list below
+#proxies = ['121.129.127.209:80', '124.41.215.238:45169', '185.93.3.123:8080', '194.182.64.67:3128', '106.0.38.174:8080', '163.172.175.210:3128', '13.92.196.150:8080']
+proxies = get_proxies()
+proxy_pool = cycle(proxies)
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="",
+)
+mycursor = mydb.cursor()
+
+mycursor.execute("CREATE DATABASE IF NOT EXISTS scrapeddata")
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="",
+  database="scrapeddata"
+)
+mycursor = mydb.cursor()
+mycursor.execute("CREATE TABLE IF NOT EXISTS data ( id int AUTO_INCREMENT PRIMARY KEY, name text, review_count text, rating text, address text, opening_hours text, website text, phone text, appointment text, covid_info text, map_link text, detais text)")
 
 
 
-options = Options()
-options.headless = True
-options.add_argument("--window-size=1920,1200")
-driver = webdriver.Chrome(options=options, executable_path="E:/task/scrapping/updated/New Folder/chromedriver")
 
 
-fields = ['Name', 'Review Count', 'Rating', 'Address','Opening Hours','Website','Phone','Appointment Link','Covid Info Link','Google Map Link'] 
 
-queries = [line.strip() for line in open("input.txt", 'r')]
+fields = ['Name', 'Review Count', 'Rating', 'Address','Opening Hours','Website','Phone','Appointment Link','Covid Info Link','Google Map Link',"details"] 
 
-for query in queries:
+dest = [line.strip() for line in open("input.txt", 'r')]
+category="dentist in "
+
+for query in dest:
 	rows=[]
-	driver.get("https://www.google.com/maps/")
+	driver=None
+	for i in range(1,11):
+		#Get a proxy from the pool
+		proxy = next(proxy_pool)
+		print(proxy)
+		print("Request #%d"%i)
+		options = Options()
+		options.headless = True
+		options.add_argument("--window-size=1920,1200")
+		# options.add_argument('--proxy-server=http://%s' % proxy)
+
+		driver = webdriver.Chrome(options=options, executable_path="E:/task/scrapping/updated/chromedriver")
+		error=0
+		try:
+			driver.get("https://www.google.com/maps/")
+		except:
+			error=1
+		if error==1:
+			continue
+		else:
+			print("SUCCESSS")
+			break
+
 	inputElement = driver.find_element("id","searchboxinput")
-	print(inputElement)
-	inputElement.send_keys(query)
+	inputElement.send_keys(category+query)
 	inputElement.send_keys(Keys.RETURN)
 	time.sleep(5)
 	source=""
@@ -51,6 +107,7 @@ for query in queries:
 		driver.get(url)
 		data=driver.page_source
 		soup = BeautifulSoup(data, 'html.parser')
+
 
 		name=""
 		review_count=""
@@ -83,6 +140,28 @@ for query in queries:
 		if soup.find("a",{"data-tooltip":"Open COVID-19 info link"}):
 			covid_link=soup.find("a",{"data-tooltip":"Open COVID-19 info link"})
 			covid_link=covid_link["href"]
+		sql = "INSERT INTO data (name, review_count,rating,address,opening_hours,website,phone,appointment,covid_info,map_link) VALUES (%s, %s,%s, %s,%s, %s,%s, %s,%s, %s)"
+		val = (name,review_count,rating,address,hours,website,phone,appointment,covid_link,url)
+		mycursor.execute(sql, val)
+		mydb.commit()
+		if soup.find("div",{"class":"y0K5Df"}):
+			ele=soup.find("div",{"class":"y0K5Df"})
+			ele.click()
+			data=driver.page_source
+			soup_v = BeautifulSoup(data, 'html.parser')
+			details=[]
+			possiblities=soup_v.find_all("li",{"class":"hpLkke"})
+			for possibility in possiblities:
+				if"WeoVJe" in possibility.get_Attribute("class").split():
+					name=possibility.find("span").text.strip()
+					details.append({"name":name,"val":0})
+				else:
+					name=possibility.find("span").text.strip()
+					details.append({"name":name,"val":1})
+			sql="INSERT INTO data(details) VALUES (%s)"
+			val=(details)
+			mycursor.execute(sql, val)
+			mydb.commit()
 		rows.append([name,review_count,rating,address,hours,website,phone,appointment,covid_link,url])
 
 		#break
